9b9d9d0e9a9b8d7d99debe43b0a3943f48f736da
PingWIN7 <m.kiss.csaba@gmail.com>
2016-12-11 19:39:21 +0000

    Added SQLManager class

---
+++ Leo/Leo/Leo/SQLManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leo
+{
+    sealed class SQLManager
+    {
+        public SQLManager()
+        {
+            //Insert strings here
+        }
+    }
+}
