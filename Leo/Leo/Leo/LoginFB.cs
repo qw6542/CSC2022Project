ad4914a6471f327182e3484398ba21102d45946c
PingWIN7 <m.kiss.csaba@gmail.com>
2016-12-11 19:33:55 +0000

    Draft classes and names with small details

---
+++ Leo/Leo/Leo/LoginFB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leo
+{
+    class LoginFB
+    {
+        public void ShareFB()
+        {
+            //This method will be changed as it need to communicate
+            //with webservers
+
+            //share something on FB
+        }
+    }
+}
---
+++ Leo/Leo/Leo/Quest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leo
+{
+    class Quest
+    {
+        public void getQuest()
+        {
+            //This method will be changed because we are waiting an answer
+            //from the server.
+        }
+
+        public void upload()
+        {
+            //This method will be changed because we are waiting an answer
+            //from the server.
+
+            //gets called if the user completed something 
+            //or uploaded a file or w\e we need
+
+        }
+
+        public void Update()
+        {
+            //This method gets the gps location, updates
+            //the GPSLocation object and refreshing the
+            //position of the map and updates Stage.
+
+            GPSLocation.update();
+            Map.update();
+            Stage.update();
+        }
+    }
+}
