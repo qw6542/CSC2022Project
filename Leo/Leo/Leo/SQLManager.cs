6bf41610a0d004112f233aa636735d8bdff46678
PingWIN7 <m.kiss.csaba@gmail.com>
2016-12-11 19:50:09 +0000

    SQL update method added

--- Leo/Leo/Leo/SQLManager.cs
+++ Leo/Leo/Leo/SQLManager.cs
@@ -10,5 +10,12 @@ namespace Leo
         {
             //Insert strings here
         }
+
+        public void Update()
+        {
+            //Updates the database with the new informations
+            //The method will be changed as it communicates with
+            //the webserver
+        }
     }
 }
