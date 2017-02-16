d1bf124412c06766276ef9a46a4561e19f44dcba
Paul Henderson <phenderson643@gmail.com>
2017-02-16 12:14:49 +0000

    integrated azure services

---
+++ v2Steven/STEVEN.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace STEVEN.Android
+{    
+    [Activity(Label = "STEVEN.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://v2steven.azurewebsites.net");
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
